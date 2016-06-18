72958d4592b8b60f5573aeb14165312df8917bc0
Markus Schanta <markus@schanta.at>
2016-06-18 09:55:39 +0100

    Preserve current working directory in GitHub scripts

--- shell/clonegist.sh
+++ shell/clonegist.sh
@@ -1,3 +1,5 @@
+pushd .
 mkdir -p ~/Coding/Gist/
 cd ~/Coding/Gist/
 curl -s https://api.github.com/users/adebar/gists | ruby -rubygems -e 'require "json"; require "shellwords"; JSON.load(STDIN.read).each { |repo| dir = (repo["description"] + " - " + repo["id"]).shellescape ; %x[ mkdir -p #{dir} ; cd #{dir} ; git clone #{repo["git_pull_url"]} . ]}'
+popd
--- shell/clonegithub.sh
+++ shell/clonegithub.sh
@@ -1,3 +1,5 @@
+pushd .
 mkdir -p ~/Coding/GitHub/
 cd ~/Coding/GitHub/
 curl -s https://api.github.com/users/adebar/repos | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
+popd
