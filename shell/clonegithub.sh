3139b2a0e93ffc7cd82f3b2476b8532ce1ed906f
Schanta <mschanta@lonmkrt0a.maninvestments.ad.man.com>
2016-07-12 18:28:01 +0100

    This seems to be a more robust URL for cloning

--- shell/clonegithub.sh
+++ shell/clonegithub.sh
@@ -1,5 +1,6 @@
+
 pushd .
 mkdir -p ~/Coding/GitHub/
 cd ~/Coding/GitHub/
-curl -s https://api.github.com/users/adebar/repos | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
+curl -s https://api.github.com/users/adebar/repos | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["clone_url"]} ]}'
 popd
