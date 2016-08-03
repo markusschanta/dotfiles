d9ff50cdf309452ddca9f8957d35c748e1bbaa83
Markus Schanta <markus@schanta.at>
2016-08-03 22:51:49 +0100

    Adding radio show to good morning script

--- shell/goodmorning.sh
+++ shell/goodmorning.sh
@@ -7,6 +7,13 @@ defaults -currentHost write com.apple.screensaver idleTime 0
 # Simulate user activity to turn on screen
 caffeinate -u -t 7200&
 
+# Set volume to 25% (4 bars)
+osascript -e "set volume output volume 25 --100%"
+# Play Ö1 Morgenjournal
+python Coding/GitHub/PyOE1/oe1.py -f Morgenjournal -u | xargs open
+# Sleep for 3 seconds (otherwise playback does not start in Chrome sometimes)
+sleep 3
+
 # Open dashboard in Chrome
 /usr/bin/osascript -e 'tell application "Google Chrome"' -e 'open location "https://www.thedash.com/dashboard/V1LwlLx7-/fullscreen"' -e "activate" -e "end tell"
 # sleep 3
